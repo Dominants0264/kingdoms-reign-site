da2e3f8d5c6be678c8bbc8be77250b458eba3049
IronspireBot <support@ironspiregames.com>
2025-07-01 09:26:50 -0300

    upgrade to full react + tailwind site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-6">
+      <h1 className="text-4xl font-bold mb-4">👑 Kingdoms Reign</h1>
+      <p className="text-lg">The ultimate MMO strategy war game. Coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
